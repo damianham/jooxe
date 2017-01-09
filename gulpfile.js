e9f17010256f5bbf327f457008cbad20c5c7adba
Damian Hamill <damianham@gmail.com>
2017-01-09 21:29:59 +0700

    run npm, bower and build in subapps

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,24 @@
 
 
 var _ = require('lodash'),
+  fs = require('fs'),
+  path = require('path'),
   gulp = require('gulp'),
   plugins = require('gulp-load-plugins')(),
   runSequence = require('run-sequence'),
   assetFiles = require('./server/assets'),
   assets = assetFiles.assets,
   testAssets = assetFiles.testAssets;
+
+var spawn = require('child_process').spawn;
+const globber = require('./server/helpers/glob_paths')
+
+function getFolders(dir) {
+  return fs.readdirSync(dir)
+    .filter(function(file) {
+      return fs.statSync(path.join(dir, file)).isDirectory();
+    });
+}
 
 // Set NODE_ENV to 'test'
 gulp.task('env:test', function () {
@@ -27,6 +39,51 @@ gulp.task('env:dev', function () {
 gulp.task('env:prod', function () {
   process.env.NODE_ENV = 'production';
 });
+
+// run npm install in each subapp
+gulp.task('npmapps', function() {
+    var files = globber('apps/*/package.json'); 
+
+    files.map(function(filename) {
+      var folder = path.dirname(filename)
+      spawn('npm', ['install'], { cwd: folder, stdio: 'inherit' })
+      .on('close', (code) => {
+        console.log('finished npm install in ', folder, 'with code',code);
+      }); 
+    });
+});
+
+// run bower install in each subapp
+gulp.task('bowerapps', function() {
+  var files = globber('apps/*/bower.json'); 
+
+  files.map(function(filename) {
+    var folder = path.dirname(filename)
+    spawn('npm', ['install'], { cwd: folder, stdio: 'inherit' })
+    .on('close', (code) => {
+      console.log('finished bower install in ', folder, 'with code',code);
+    }); 
+  });
+});
+
+// build the web distribution in each subapp
+// i.e. minify/uglify and package into single js/css files
+gulp.task('buildapps', function() {
+  var files = globber('apps/*/gulpfile.js'); 
+
+  files.map(function(filename) {
+    var folder = path.dirname(filename)
+    spawn('gulp', ['build'], { cwd: folder, stdio: 'inherit' })
+    .on('close', (code) => {
+      console.log('finished build in ', folder, 'with code',code);
+    }); 
+  });
+});
+
+gulp.task('build', function (done) {
+  runSequence('npmapps',  'bowerapps', 'buildapps', done);
+});
+
 
 // Nodemon task
 gulp.task('nodemon', function () {
